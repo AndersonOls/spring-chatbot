76991b3030a6c3867f57d92604a1b3e9478bdd5b
Anderson <anderson.silva1996@hotmail.com>
2024-03-17 14:36:10 -0300

    feat: add cors configuration

--- src/main/java/com/anderson/chatbot/controllers/FaqController.java
+++ src/main/java/com/anderson/chatbot/controllers/FaqController.java
@@ -5,10 +5,7 @@ import com.anderson.chatbot.dto.MessageResponse;
 import com.anderson.chatbot.dto.MessageRequest;
 import com.anderson.chatbot.services.FaqService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
@@ -18,7 +15,6 @@ public class FaqController {
     public FaqController(FaqService faqService){
         this.faqService = faqService;
     }
-
     @PostMapping
     public ResponseEntity<MessageResponse> answerQuestion(@RequestBody MessageRequest request){
         String answer = this.faqService.getAnswer(request.message());
